fcef9d7c4b90a28832679cd28e162081b734b5cc
laithkh12 <168769648+laithkh12@users.noreply.github.com>
2025-05-11 15:53:26 +0200

    Add files via upload

---
+++ weather-app/src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import WeatherApp from "./components/WeatherApp/WeatherApp";
+import "./components/WeatherApp/WeatherApp.css";
+
+const App = () => {
+  return (
+    <div>
+      <WeatherApp />
+    </div>
+  );
+};
+
+export default App;
